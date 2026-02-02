7360d551533c44ea04a2781cd8150238d1023422
Victor Souza <vicentesouzavictor29@gmail.com>
2026-02-02 14:44:38 -0300

    criando um if else para mudar conforme ação do usuário

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ function alterarStatus(id) {
 
     if (imagem.classList.contains("dashboard__item__img--rented")) {
         imagem.classList.remove("dashboard__item__img--rented");
-        botap.classList.remove("dashboard__item__button--return");
+        botao.classList.remove("dashboard__item__button--return");
+        botao.textContent = "Alugar";
+    } else {
+        imagem.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        botao.textContent = "Devolver";
     }
 }
